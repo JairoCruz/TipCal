5c55b79e542a760b090512a03b82c5604fb06712
JairoCruz <azaveliz@gmail.com>
2016-05-31 14:33:43 -0600

    implementacion de ButterKnife y el desarrollo de un metodo siempre utilizando las bondades de ButterKnife y sus bind o enlaces o inyecciones

--- app/src/main/java/edu/jairo/android/tipcal/MainActivity.java
+++ app/src/main/java/edu/jairo/android/tipcal/MainActivity.java
@@ -1,18 +1,47 @@
 package edu.jairo.android.tipcal;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Para poder utilizar el plugin de ButterKnife es necesario seleccionar el elemento R.layout.activity_main, luego alt + insert
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Con esto hago una inyeccion de todos los componentes que utilizo
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -21,14 +50,31 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-   
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_about){
+        if (item.getItemId() == R.id.action_about) {
             about();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // ButterKnife tambien me permite inyectar un evento click sobre un elemento button
+    // con la siguiente notacion
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmit(){
+        Log.e(getLocalClassName(), "click en submit");
+        // ButterKnife me implemeta un Listener sin necesidad que yo haga el <code></code>
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        try {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }catch (NullPointerException npe){
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
     }
 
     private void about() {
