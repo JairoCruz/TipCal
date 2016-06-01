f5fc847d4c0bde9838df3b00dea06d6c90a615a9
JairoCruz <azaveliz@gmail.com>
2016-06-01 11:44:13 -0600

    ahora agrego una nueva actividad y layout para mostrar un detalle de la propina. aun no se ha agregado la funcionabilidad

---
+++ app/src/main/java/edu/jairo/android/tipcal/activities/TipDetailActivity.java
@@ -0,0 +1,15 @@
+package edu.jairo.android.tipcal.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.jairo.android.tipcal.R;
+
+public class TipDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_detail);
+    }
+}
